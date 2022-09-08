27834ebe63b01c3655567de3d300bf600a008feb
illak <i.zapata1989@gmail.com>
2022-09-08 16:14:25 -0300

    update

--- main.R
+++ main.R
@@ -1,5 +1,6 @@
 library(tidyverse)
-
+library(ggtext)
+library(glue)
 
 datos <- read_csv("data/datos.csv")
 
@@ -26,7 +27,12 @@ datos_categorias <- datos %>%
       cohorte_anio %in% c(2018,2019) ~ "P2",
       cohorte_anio %in% c(2020,2021) ~ "P3",
       cohorte_anio %in% c(2022,2023) ~ "P4"),
-    periodo = factor(periodo, levels = c("P1","P2","P3","P4"))
+    periodo = factor(periodo, levels = c("P1","P2","P3","P4")),
+    periodo_anio = case_when(
+      periodo=="P1" ~ "2016-2017",
+      periodo=="P2" ~ "2018-2019",
+      periodo=="P3" ~ "2020-2021",
+      periodo=="P4" ~ "2022-2023")
   ) %>% 
   mutate(
     categoria = case_when(
@@ -48,8 +54,21 @@ datos_categorias <- datos %>%
     categoria = as.factor(categoria)
   )
 
-datos_categorias %>% 
-  group_by(periodo, categoria) %>% 
+
+(recurrencia_periodos_plot <- datos_categorias %>% 
+  mutate(periodo_mod = glue("<b>{periodo}</b><br><i>{periodo_anio}</i>")) %>% 
+  group_by(periodo_mod, categoria) %>% 
   summarise(n = n_distinct(idpersona)) %>% 
-  ggplot(aes(x = periodo, y = n, fill = categoria)) +
-  geom_col()
+  ggplot(aes(x = periodo_mod, y = n, fill = categoria)) +
+  geom_col(width = .5) +
+  ggthemes::scale_fill_tableau() +
+  labs(y = NULL, fill = "Categoría", x = "Periodo") +
+  theme_minimal(base_size = 18) +
+  theme(
+    axis.text.x = element_markdown()
+  ))
+
+ggsave("plot1.png", width = 10, height = 10, dpi = 320)
+
+
+(recurrencia_)
